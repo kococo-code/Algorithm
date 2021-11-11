6f7975f510655c67ab76378418a3895ce4a5bce5
임진택 <kococo9504@gmail.com>
2021-11-11 23:46:02 +0900

    64 Minimum Path Sum with DP

---
+++ LeetCode/64_min_path_sum.js
@@ -0,0 +1,45 @@
+/**
+ * @param {number[][]} grid
+ * @return {number}
+ */
+
+var minPathSum = function (grid) {
+  const width = grid[0].length;
+  const height = grid.length;
+  const dp = new Array(height).fill(Infinity).map(() => {
+    return new Array(width).fill(Infinity);
+  });
+  dp[0][0] = grid[0][0];
+  if (width === 0 && height === 0) return dp[0][0];
+  /*
+    function moveCell(row,col,direction="None"){
+        if(row > height -1 || col > width -1) return;
+        if(direction === 'right'){
+            dp[row][col] = Math.min(dp[row][col-1] + grid[row][col], dp[row][col])
+        }
+        if(direction === 'down'){
+            dp[row][col] = Math.min(dp[row-1][col] + grid[row][col], dp[row][col])
+        }
+        if(row === height -1 && col === width -1){ return;
+            //dp[row][col] = Math.min(dp[row][col-1], dp[row-1][col]) + grid[row][col]
+        }
+        // Move Right;
+        moveCell(row, col +1,"right")
+        // Move Down;
+        moveCell(row +1, col,"down")        
+    }
+    moveCell(0,0)
+    */
+  for (let i = 1; i < height; i++) {
+    dp[i][0] = dp[i - 1][0] + grid[i][0];
+  }
+  for (let j = 1; j < width; j++) {
+    dp[0][j] = dp[0][j - 1] + grid[0][j];
+  }
+  for (let i = 1; i < height; i++) {
+    for (let j = 1; j < width; j++) {
+      dp[i][j] = Math.min(dp[i - 1][j], dp[i][j - 1]) + grid[i][j];
+    }
+  }
+  return dp[height - 1][width - 1];
+};
